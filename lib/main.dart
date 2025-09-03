2c41266494425a9c2922f81977d59e12082604a2
nguyenvankien <anhkien7780@gmail.com>
2025-09-03 14:35:07 +0700

    feat: Integrate Firebase Core and Messaging
    This commit integrates Firebase Core and Firebase Messaging into the application.
    
    Key changes include:
    - Added Firebase Core and Firebase Messaging dependencies to `pubspec.yaml`.
    - Configured Firebase for Android:
        - Added `google-services.json` file.
        - Updated `android/app/build.gradle.kts` and `android/build.gradle.kts` to include the Google Services plugin.
        - Updated `android/settings.gradle.kts` with the Google Services plugin.
    - Generated `firebase.json` and `lib/firebase_options.dart` for Firebase configuration.
    - Initialized Firebase in `lib/main.dart` for Android platform and retrieved FCM token.
    - Updated plugin registration for macOS and Windows to include Firebase Core and Firebase Messaging.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:todo_app_bloc/configs/app_configs.dart';
@@ -11,5 +16,13 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   await SupabaseServices.supabaseInit();
+  if (Platform.isAndroid) {
+    await Firebase.initializeApp();
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+    String? token = await messaging.getToken();
+    if(token != null){
+      log("Token: ${token.toString()}");
+    }
+  }
   runApp(const MyApp());
 }
