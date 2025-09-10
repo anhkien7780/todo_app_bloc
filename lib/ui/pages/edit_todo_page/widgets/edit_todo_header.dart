25b8f440c14a09573ffbebd11219e8445e763f17
Nguyễn Văn Kiên <anhkien7780@gmail.com>
2025-09-11 02:23:03 +0700

    refactor: Introduce BaseHeader widget
    This commit introduces a new `BaseHeader` widget for consistent header styling across the application.
    
    - Created `BaseHeader` with `title` and `leadingButton` properties. It includes the common background and ellipse decorations.
    - Refactored `EditTodoHeader` to utilize the new `BaseHeader` widget, simplifying its implementation.
    - Updated SVG assets for US and Vietnamese flags (`ic_us_flag.svg`, `ic_vn_flag.svg`) to use a rectangular shape instead of a circular one.

--- lib/ui/pages/edit_todo_page/widgets/edit_todo_header.dart
+++ lib/ui/pages/edit_todo_page/widgets/edit_todo_header.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app_bloc/common/app_colors.dart';
 import 'package:todo_app_bloc/common/app_dimens.dart';
 import 'package:todo_app_bloc/common/app_svgs.dart';
-import 'package:todo_app_bloc/common/app_text_styles.dart';
 import 'package:todo_app_bloc/generated/l10n.dart';
 import 'package:todo_app_bloc/model/enums/edit_todo_page_mode.dart';
+import 'package:todo_app_bloc/ui/widgets/headers/base_header.dart';
 import 'package:todo_app_bloc/ui/widgets/images/svg_image.dart';
 
 class EditTodoHeader extends StatelessWidget {
@@ -19,47 +18,11 @@ class EditTodoHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      fit: StackFit.loose,
-      alignment: Alignment.center,
-      children: [
-        Container(
-          width: double.infinity,
-          height: AppDimens.addTodoPageHeaderHeight,
-          color: AppColors.primary,
-        ),
-        _buildEllipse1(),
-        _buildEllipse2(),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            _buildCloseButton(),
-            Text(
-              mode == EditTodoPageMode.add
-                  ? S.of(context).addNewTask
-                  : S.of(context).editTask,
-              style: AppTextStyles.whiteS16SemiBold,
-            ),
-            const SizedBox(width: AppDimens.circleButtonSize),
-          ],
-        ),
-      ],
-    );
-  }
-
-  Positioned _buildEllipse1() {
-    return Positioned(
-      top: -48,
-      left: -191,
-      child: SVGImage(imageUri: AppSvgs.icEllipse1),
-    );
-  }
-
-  Positioned _buildEllipse2() {
-    return Positioned(
-      top: -27,
-      right: -82,
-      child: SVGImage(imageUri: AppSvgs.icEllipse2),
+    return BaseHeader(
+      title: mode == EditTodoPageMode.add
+          ? S.of(context).addNewTask
+          : S.of(context).editTask,
+      leadingButton: _buildCloseButton(),
     );
   }
 
---
+++ lib/ui/widgets/headers/base_header.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app_bloc/common/app_colors.dart';
+import 'package:todo_app_bloc/common/app_dimens.dart';
+import 'package:todo_app_bloc/common/app_svgs.dart';
+import 'package:todo_app_bloc/common/app_text_styles.dart';
+import 'package:todo_app_bloc/ui/widgets/images/svg_image.dart';
+
+class BaseHeader extends StatelessWidget {
+  final String? title;
+  final Widget? leadingButton;
+
+  const BaseHeader({
+    super.key,
+    this.title,
+    this.leadingButton,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.loose,
+      alignment: Alignment.center,
+      children: [
+        Container(
+          width: double.infinity,
+          height: AppDimens.addTodoPageHeaderHeight,
+          color: AppColors.primary,
+        ),
+        _buildEllipse1(),
+        _buildEllipse2(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            leadingButton ?? const SizedBox.shrink(),
+            Text(title ?? "", style: AppTextStyles.whiteS16SemiBold),
+            const SizedBox(width: AppDimens.circleButtonSize),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Positioned _buildEllipse1() {
+    return Positioned(
+      top: -48,
+      left: -191,
+      child: SVGImage(imageUri: AppSvgs.icEllipse1),
+    );
+  }
+
+  Positioned _buildEllipse2() {
+    return Positioned(
+      top: -27,
+      right: -82,
+      child: SVGImage(imageUri: AppSvgs.icEllipse2),
+    );
+  }
+}
